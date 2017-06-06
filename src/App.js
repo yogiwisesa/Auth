0b3092d555d0f36ec5811e9d71a9bc935bff1370
DESKTOP-EDV0F0U\Yogi Wisesa <yogiwisesa@gmail.com>
2017-06-07 01:51:30 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+    state = { loggedIn: 'null' };
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: '',
+            authDomain: '',
+            databaseURL: '',
+            projectId: '',
+            storageBucket: '',
+            messagingSenderId: ''
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Button onPress={() => firebase.auth().signOut()} >Log Out</Button>
+                        </CardSection>
+                    </Card>
+                );
+            case false:
+                return (
+                    <LoginForm />
+                );
+            default:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Spinner size="large" />
+                        </CardSection>
+                    </Card>
+                );
+
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+                {this.renderContent()}
+            </View>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
+
+//Biar import component2 yang sering di pkai gampang, 
+// jadi di parent tinggal pakai import { Nama Komponen } from nama folder
+// Untuk component yang digunakan tidak bisa export default
